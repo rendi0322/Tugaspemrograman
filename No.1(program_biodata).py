16a211eb3030fa29ebf1c87f74dc660363f9dd86
rendi0322 <114746077+rendi0322@users.noreply.github.com>
2022-10-27 23:13:56 +0700

    Rename Nama,kelas to No.1(program_biodata).py

---
+++ No.1(program_biodata).py
@@ -0,0 +1,7 @@
+nama=input("masukan nama:")
+kelas=input("masukan kelas:")
+prodi=input("masukan prodi:")
+umur=input("masukan umur:")
+hobi=input("masukan hobi:")
+
+print("hallo perkenalkan nama saya{},kelas{},program studi{}.saat ini saya berumur{}tahun dan hobi saya adalah{}".format(nama,kelas,prodi,umur,hobi))
